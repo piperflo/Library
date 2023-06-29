c87f346e832b643ece3b6b32d103f1c8c9393bc1
piperflo <69817139+piperflo@users.noreply.github.com>
2023-06-29 18:29:51 -0500

    Updated inputs/urls

--- Library.js
+++ Library.js
@@ -1,51 +1,3 @@
-/*
-//one way to create objects
-const myObj = {
-    property: 'Value!',
-    otherProperty: 77,
-    "function": function(){
-        //function
-    }
-}
-console.log(myObj.property);
-
-
-
-//another way 
-const playerOneName = "tim";
-const playerTwoName = "jim";
-const playerOneMarker = "X";
-const playerTwoMarker = "O";
-
-const playerOne = {
-    name: "tim",
-    mark: "X"
-}
-
-const playerTwo = {
-    name: "jim",
-    mark: "O"
-}
-
-//Another way
-function Player(name, marker){
-    this.name = name;
-    this.marker = marker;
-    this.sayName = function() {
-        console.log(name);
-    }
-}
-
-const player = new Player('steve', 'X');
-//console.log(player.name)
-
-
-player.sayName();
-function gameOver(winningPlayer){
-    console.log("Congratulations");
-    console.log(winningPlayer.name + " is the winner!");
-}
-*/
 const modalForm = document.getElementById("modal-form");
 const btn = document.getElementById("add-book-form");
 const addBook = document.getElementById("add-book");
@@ -67,7 +19,6 @@ function book(url, title, author, pages, read){
     else{
         this.read = "Not Finished"
     }
-    //console.log(title + " by " + author + ", " + pages + " pages, " + read);
     this.info = function(){
         return title + " by " + author + ", " + pages + " pages, " + this.read;
     }
@@ -76,7 +27,6 @@ function book(url, title, author, pages, read){
 function addBookToLibrary(event){
     event.preventDefault();
     console.log("Entered addBookToLibrary");
-    //const container = document.querySelector('#books');
     var title = document.getElementById("input-title").value;
     var author = document.getElementById("input-author").value;
     var pages = document.getElementById("input-pages").value;
@@ -84,18 +34,18 @@ function addBookToLibrary(event){
     console.log(title);
     console.log(author);
     console.log(pages);
-    //const read = document.getElementById("yes-no");
     var read = "";
     if(document.getElementById('yes').checked) {     
         read += "Finished";
       }else {
         read += "Not Finished";
     }
-    console.log(read);
+    if (!url) {
+        url = "images/help.svg"
+    }
     createBook(url, title, author, pages, read);
     const Book = new book(url, title, author, pages, read);
     myLibrary.push(Book);
-    //event.preventDefault();
 }
 
 function createBook(url, title, author, pages, read){
@@ -144,9 +94,6 @@ addBook.addEventListener("click", addBookToLibrary, false);
 
 const harryPotter = new book("https://wallpapercave.com/wp/wp6289310.jpg", "Harry Potter and the sorcerer's stone", "J.K. Rowling", 309, "yes");
 myLibrary.push(harryPotter);
-//console.log(harryPotter.info());
-
-
 
 myLibrary.forEach(function (book, index) {
     createBook(book.url, book.title, book.author, book.pages, book.read);
