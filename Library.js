0076cb1d1054a1b0622f551032a4e6a75d71a573
piperflo <69817139+piperflo@users.noreply.github.com>
2023-06-30 17:51:54 -0500

    Updated Delete Functionality

--- Library.js
+++ Library.js
@@ -2,8 +2,8 @@ const modalForm = document.getElementById("modal-form");
 const btn = document.getElementById("add-book-form");
 const addBook = document.getElementById("add-book");
 const books = document.getElementById("books");
-
-let playerScore = 0;
+const del = document.querySelector('.delete');
+let bookCount = 0;
 var close = document.getElementsByClassName("close")[0];
 let myLibrary = [];
 
@@ -46,39 +46,71 @@ function addBookToLibrary(event){
     createBook(url, title, author, pages, read);
     const Book = new book(url, title, author, pages, read);
     myLibrary.push(Book);
+    //console.log(bookCount);
+    //bookCount++;
 }
 
 function createBook(url, title, author, pages, read){
     console.log("Entered createBook");
     const book = document.createElement('div');
+    book.setAttribute('class', 'book');
+    book.setAttribute('id', title);
+    book.setAttribute('data-index', bookCount);
+    //book.setAttribute('onclick', "deleteBook(event, book, bookCount);");
 
     this.title = document.createElement('span');
-    this.title.setAttribute('id', 'title');
+    this.title.setAttribute('class', 'title');
     this.title.textContent = "Title: " + title;
 
     this.author = document.createElement('span');
-    this.author.setAttribute('id', 'author');
+    this.author.setAttribute('class', 'author');
     this.author.textContent = "Author: " + author;
 
     this.pages = document.createElement('span');
-    this.pages.setAttribute('id', 'pages');
+    this.pages.setAttribute('class', 'pages');
     this.pages.textContent = "Pages: " + pages;
 
     this.read = document.createElement('span');
-    this.read.setAttribute('id', 'read');
+    this.read.setAttribute('class', 'read');
     this.read.textContent = "Finished: " + read;
+    this.read.setAttribute('data-status', "status");
+
 
     this.url = document.createElement('img');
-    this.url.setAttribute('id', 'url');
+    this.url.setAttribute('class', 'url');
+    this.url.alt = title + " image";
     this.url.src = url;
 
-    book.setAttribute('class', 'book');
+    this.delete = document.createElement('button');
+    this.delete.setAttribute('class', 'delete');
+    this.delete.textContent = "Delete Book";
+    this.delete.setAttribute('onclick', "deleteBook(event, this);");
+
+    /*
+    const status = document.createElement('button');
+    status.setAttribute('class', 'status');
+    status.textContent = "Change Status";
+    status.setAttribute('onclick', "changeStatus(event, this);");
+*/
     book.appendChild(this.url);
     book.appendChild(this.title);
     book.appendChild(this.author);
     book.appendChild(this.pages);
     book.appendChild(this.read);
+    book.appendChild(this.delete);
+    //book.appendChild(status);
     books.appendChild(book);
+    bookCount++;
+}
+function deleteBook(event, b){
+    const book = b.parentNode;
+    console.log(book);
+    let index = book.getAttribute('data-index');
+    let id = book.getAttribute('id');
+    const element = document.getElementById(id);
+    element.remove();
+    myLibrary.splice(index, 1);
+    bookCount--;
 }
 
 btn.onclick = function() {
